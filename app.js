e3ff4b0aa299fb59e950c53c30d9534ecd52f319
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-03-09 03:58:47 -0500

    Enable socketing and first message reaction

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const { App } = require('@slack/bolt');
 require('dotenv').config()
 
 // Initializes your app with your bot token and signing secret
+const port = 3000;
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || port
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
 });
 
 (async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  // Start your app
+  await app.start();
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
