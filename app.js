965d3c77bd50aafba981c1c1cd423ec8984673b1
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-03-09 20:34:52 -0500

    Fixed random image reponse

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import slackBolt from '@slack/bolt';
 import nodeFetch from 'node-fetch';
+import _ from 'lodash';
 import { createApi } from 'unsplash-js';
 import 'dotenv/config';
 
@@ -8,8 +9,6 @@ const unsplash = createApi({
   accessKey: process.env.UNSPLASH_ACCESS_KEY,
   fetch: nodeFetch,
 });
-
-console.log("hello");
 
 // Initializes your app with your bot token and signing secret
 const port = 3000;
@@ -25,36 +24,40 @@ const app = new App({
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-  console.log("hello in message");
   // say() sends a message to the channel where the event was triggered
   await say(`Hey there, <@${message.user}>!`);
 });
 
 // Listens to incoming messages that contain "giraffe"
 app.message('giraffe', async ({ message, say }) => {
-  console.log("hello in giraffe");
-  // say() sends a message to the channel where the event was triggered
-  const giraffeImage = await unsplash.photos.getRandom('giraffe');
-  console.log(giraffeImage.urls.full);
-  console.log(giraffeImage);
-  await say({
-    'text': 'a giraffe image',
-    'blocks': [
-      {
-        'type': 'section',
-        'block_id': 'section1',
-        'text': {
-          'type': 'mrkdwn',
-          'text': `Have a giraffe, <@${message.user}>!.`,
-        },
-        'accessory': {
-          'type': 'image',
-          'image_url': giraffeImage.urls.full, //'https://source.unsplash.com/random?giraffe',
-          'alt_text': 'giraffe',
-        },
-      },
-    ],
-  });
+  unsplash.photos
+    .getRandom({ query: 'giraffe', count: 1 })
+    .then((result) => {
+      console.log(result.response[0].urls.raw);
+      const photo = result.response[0].urls.raw;
+
+      say({
+        'text': 'a giraffe image',
+        'blocks': [
+          {
+            'type': 'section',
+            'block_id': 'section1',
+            'text': {
+              'type': 'mrkdwn',
+              'text': `Have a giraffe, <@${message.user}>!.`,
+            },
+            'accessory': {
+              'type': 'image',
+              'image_url': photo, //'https://source.unsplash.com/random?giraffe',
+              'alt_text': 'giraffe',
+            },
+          },
+        ],
+      });
+    })
+    .catch(() => {
+      console.log('something went wrong while getting a giraffe!');
+    });
 });
 
 (async () => {
